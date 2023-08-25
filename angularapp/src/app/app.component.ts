5e5bc40fa6e3eb227af8f587bf0109e5cc545a46
Iamneo <techadmin@iamneo.ai>
2023-08-25 09:58:29 +0000

    Backup Commit

--- angularapp/src/app/app.component.ts
+++ angularapp/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularapp';
+  value1: string = ''; 
+  value2: string = ''; 
+  result: string =''; 
+  r1:string="";
+  amount1: number = 0; 
+  today=new Date(); 
+  handle(sortoption1: any,sortoption2: any,amount1: any){
+    console.log(sortoption1,sortoption2,amount1);
+this.result=(Math.round((sortoption2/sortoption1)*amount1)).toFixed(2)
+      this.r1=this.result;
+
+      this.r1=this.result;
+     
+  }
 }
